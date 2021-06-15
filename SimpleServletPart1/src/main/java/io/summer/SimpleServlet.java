7ca76a7a56f6e6445a5e67b1708fe7406dda9418
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-15 17:22:55 +0530

    First Servlet

---
+++ SimpleServletPart1/src/main/java/io/summer/SimpleServlet.java
@@ -0,0 +1,58 @@
+package io.summer;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(description = "A simple servlet", urlPatterns = { "/SimpleServletPath" })
+/*
+	base URL (web-application name) with matching urlPattern - makes tomcat run this class
+	
+	Default method - doGet
+*/ 
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public SimpleServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("Hello from doGet");
+		
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h3>Hello From</h3><h1>doGet</h1>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("Hello from doGet");
+		
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+	}
+
+}
