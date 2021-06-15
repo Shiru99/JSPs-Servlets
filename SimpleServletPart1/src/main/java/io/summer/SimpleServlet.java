dcc21f57d2c561cea0b411255e94d246e0e24766
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-15 18:35:00 +0530

    Servlet using Annotations

--- SimpleServletPart1/src/main/java/io/summer/SimpleServlet.java
+++ SimpleServletPart1/src/main/java/io/summer/SimpleServlet.java
@@ -12,47 +12,29 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SimpleServlet using Annotations
  */
-@WebServlet(description = "A simple servlet", urlPatterns = { "/SimpleServletPath" })
+
 /*
 	base URL (web-application name) with matching urlPattern - makes tomcat run this class
 	
 	Default method - doGet
 */ 
+@WebServlet(description = "A simple servlet Using Annotations", urlPatterns = { "/SimpleServletPath" })
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public SimpleServlet() {
-        // TODO Auto-generated constructor stub
-    }
+    public SimpleServlet() {}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		System.out.println("Hello from doGet");
-		
-//		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.setContentType("text/html");
-		PrintWriter writer = response.getWriter();
-		writer.println("<h3>Hello From</h3><h1>doGet</h1>");
-		
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		System.out.println("Hello from doGet");
+		System.out.println("A simple servlet Using Annotations : Hello from doGet");
 		
 		// TODO Auto-generated method stub
-		doGet(request, response);
-		
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h3>Hello From</h3><h1>doGet</h1><p>A simple servlet Using Annotations</p>");
 	}
-
 }
