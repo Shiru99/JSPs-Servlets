fffc841e22a32f09b807bcb57f7ceac680da6d32
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-16 20:43:56 +0530

    Servlet : Request,Session & Context

---
+++ ServletSessionAndContext/src/main/java/io/summer/ServletWithGetParameters.java
@@ -0,0 +1,56 @@
+package io.summer;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+@WebServlet(description = "Servlet : Request, session & context objects", urlPatterns = { "/Scopes" })
+public class ServletWithGetParameters extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ServletWithGetParameters() {    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException 
+	{	
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		
+		
+		// http://localhost:8989/ServletSessionAndContext/Scopes?user=John+Doe
+		String userName = request.getParameter("user");
+		
+		
+		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
+		
+		if(context.getAttribute("userCount")==null) {
+			context.setAttribute("userCount", 0);
+		}
+		
+		
+		if(userName!=null && userName!=""){
+			session.setAttribute("userName", userName);
+			context.setAttribute("userName", userName);
+			int userCount = (int)context.getAttribute("userCount");
+			context.setAttribute("userCount", ++userCount);
+		}
+		
+		
+		writer.println("<h3>Request :</h3>"+"<h4>Hello "+userName+"</h4>");
+		writer.println("<br><h3>Session :</h3>"
+						+"<h4>Hello "+(String) session.getAttribute("userName")
+						+"</h4>");
+		writer.println("<br><h3>Context :</h3>"
+						+"<h4>Hello "+(String) context.getAttribute("userName")
+						+"</h4>"+"<br>Total User Count : "+context.getAttribute("userCount"));
+	}
+}
