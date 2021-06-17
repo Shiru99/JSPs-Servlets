79a654b37e0b6f46c4d2e45a5edd012332c3e6fb
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-17 08:50:43 +0530

    ServletConfig vs ServletContext

---
+++ Servlets - Life Cycle/src/main/java/io/summer/Applicant.java
@@ -0,0 +1,23 @@
+package io.summer;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Applicant extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException 
+	{
+		String email = getServletConfig().getInitParameter("Email");
+		String website = getServletContext().getInitParameter("Website-name");
+		
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("<center><h1>" + website + "</h1><p>Contact us: " + email+"</center>");
+	}
+}
