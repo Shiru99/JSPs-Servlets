2f2c4deef903e58f4a1892736e419b316b04c3c1
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-19 20:06:58 +0530

    model / DTO

---
+++ MVC-Application/src/main/java/io/summer/dto/User.java
@@ -0,0 +1,22 @@
+package io.summer.dto;
+
+// dto - data transfer object (model)
+
+
+public class User {
+	String userName;
+	String fullName;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}	
+}
