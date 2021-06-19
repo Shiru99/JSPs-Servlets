b795febe328a158996c3d9c4116a29461163c2da
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-19 20:05:43 +0530

    Bussiness Service

---
+++ MVC-Application/src/main/java/io/summer/service/LoginService.java
@@ -0,0 +1,36 @@
+package io.summer.service;
+
+import java.util.HashMap;
+
+import io.summer.dto.User;
+
+
+public class LoginService {
+	
+	HashMap<String, String> users = new HashMap<String, String>();
+	
+	
+	public LoginService() {
+		users.put("John Doe", "Johnny d’Eu");
+		users.put("Jene Doe", "Jenny d’Eu");
+	}
+
+	public boolean authenticate(String userName, String password) {
+
+		if (userName.trim().isEmpty() || userName == null || password.trim().isEmpty() || password == null) {
+			return false;
+		}
+
+		return true;
+	}
+	
+	public User getUser(String userName) {
+		User user = new User();
+		
+		user.setUserName(userName);
+		user.setFullName(users.get(userName));
+		return user;
+		
+	}
+}
+
